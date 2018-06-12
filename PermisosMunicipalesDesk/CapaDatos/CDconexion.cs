56dbf50e1a32c203d280eaa6dfa9bdc66b7dc39d
Joseph <jose.perez@peluka.info>
2018-06-12 07:04:52 -0400

    implementacion metodo para saber el nombre del usuario logueado. falta implementar singleton

--- PermisosMunicipalesDesk/CapaDatos/CDconexion.cs
+++ PermisosMunicipalesDesk/CapaDatos/CDconexion.cs
@@ -45,9 +45,52 @@ namespace CapaDatos
             OracleDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                Console.WriteLine(reader.GetValue(0));
+                //Console.WriteLine("Tipo User Cod: "+reader.GetValue(0));
                 resultado = resultado + reader.GetValue(0);
             }
+            return resultado;
+        }
+        public string obtenerDatosUser(string user)
+        {
+            cmd.Connection = Ora;
+            string resultado = "";
+            int i = 0;
+            cmd.CommandText = "Select * from PERSONAL where usuario='" + user+"'";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetValue(i));
+                resultado = resultado +","+ reader.GetValue(0);
+                i = i+1;
+            }
+            reader.Close();
+            return resultado;
+        }
+
+        public string obtenerNombreUser(string user)
+        {
+            string queryString = "Select * from PERSONAL where usuario='" + user + "'";
+            
+                OracleCommand command = new OracleCommand(queryString, Ora);
+                string resultado = "";
+               // int i = 0;
+                OracleDataReader reader;
+                reader = command.ExecuteReader();
+
+                // Always call Read before accessing data.
+                while (reader.Read())
+                {
+                    //Console.WriteLine(reader.GetValue(i));
+                    resultado = reader.GetValue(1)+" "+reader.GetValue(2)+" "+reader.GetValue(3);
+                }
+
+                // Always call Close when done reading.
+                reader.Close();
+                cmd.Connection = Ora;
+                reader.Close();
+
+            
             return resultado;
         }
 
